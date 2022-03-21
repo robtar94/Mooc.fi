7546ddc9befccfbf8d7701cc9b94cd9fa257ab64
Robert Tarnas <robert.tarnas@protonmail.com>
2022-03-21 16:26:39 +0100

    finished part 1

--- Java Programming I/TMCProjects/mooc-java-programming-i/part01-Part01_34.Same/src/main/java/Same.java
+++ Java Programming I/TMCProjects/mooc-java-programming-i/part01-Part01_34.Same/src/main/java/Same.java
@@ -6,6 +6,16 @@ public class Same {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here. 
+        System.out.println("Enter the first string:");
+        String fst = scan.nextLine();
+        System.out.println("Enter the second string:");
+        String sec = scan.nextLine();
+        if(fst.equals(sec)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
+
+
     }
 }
