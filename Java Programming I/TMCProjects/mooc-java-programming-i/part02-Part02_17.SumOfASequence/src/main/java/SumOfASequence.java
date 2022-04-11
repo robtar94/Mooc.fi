f8aa8f603e2ed1800d63117d076d348eaf37c6ec
Robert Tarnas <robert.tarnas@protonmail.com>
2022-04-11 12:17:42 +0200

    2.3 More loops

--- Java Programming I/TMCProjects/mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ Java Programming I/TMCProjects/mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -5,6 +5,14 @@ public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int result = 0;
+        System.out.print("Last number? ");
+        int n = Integer.valueOf(scanner.nextLine());
+
+        for(int i = 1; i <= n; i++) {
+            result+=i;
+        }
+        System.out.println(result);
 
     }
 }
--- Java Programming I/TMCProjects/mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ Java Programming I/TMCProjects/mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -5,6 +5,20 @@ public class SumOfASequenceTheSequel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int result = 0;
+        int first, last = 0;
+
+        System.out.print("First number? ");
+        first = Integer.valueOf(scanner.nextLine());
+        System.out.print("Last number? ");
+        last = Integer.valueOf(scanner.nextLine());
+
+        for (int i = first; i <= last; i++) {
+
+            result = result + i;
+        }
+
+        System.out.println(result);
 
     }
 }
